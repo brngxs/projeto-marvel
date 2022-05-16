3b28851f114f2b5252808e66fa8d5a54cda92e9f
Breno Guimarães <103603812+brngxs@users.noreply.github.com>
2022-05-16 00:10:44 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+/*
+OBJETIVO 1: MOUSE EM CIMA DO PERSONAGEM TEMOS:
+ 
+OBJETIVO 2: TROCAR A IMAGEM E NOME DO PERSONAGEM GRANDE
+*/
+
+
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+    const idSelecionado = personagem.attributes.id.value;
+
+    if(idSelecionado === 'ultron'){
+        return;
+    }
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        
+        personagem.classList.add('selecionado');
+
+
+const imagemJogador1 = document.getElementById('personagem-jogador-1');
+imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+
+const nomeJogador1 = document.getElementById('nome-jogador-1');
+const nomeSelecionado = personagem.getAttribute('data-name');
+
+nomeJogador1.innerHTML = nomeSelecionado;
+
+    })
+})
+
